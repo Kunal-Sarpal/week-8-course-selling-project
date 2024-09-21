1067e56fe9b0d5069b2bd0625691d00812167b99
Kunal-Sarpal <sarpalkunal7@gmail.com>
2024-09-21 21:33:02 +0530

    --

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
+const { mongoose } = require("mongoose");
 const app = express();
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 
+async function main(){
+    await mongoose.connect("mongodb+srv://100xdevs:WvaTca0509mb90YX@cluster0.ossjd.mongodb.net/coursera-app")
+    app.listen(3000);
 
-app.listen(3000);
+}
+main();
